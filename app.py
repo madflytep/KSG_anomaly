d37c8e24fbfdc2d150ff4c3a3d6723d1bd23a972
Ширшов Александр Юрьевич <a.shirshov@ano.local>
2023-07-31 17:08:09 +0300

    Added project

---
+++ app.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import xlsxwriter
+from io import BytesIO
+import base64
+
+from modules import upload_ksg_1, show_plot_1
+
+# st.text("This is data anomaly KSG service")
+
+# st.write("**This is just**")
+
+# df=pd.read_csv()
+
+#отображение графиков
+
+# fig, ax = plt.subplots()
+# ax.scatter(np.arange(5), np.arange(5)**2)
+
+# st.write(fig)
+
+# заголовки и подзаголовки
+st.title("Сервис по поиску аномалий данных в КСГ")
+st.header("Пожалуйста, загрузите текущий файл КСГ")
+# st.subheader("Subs")
+
+#отображение метрик
+
+# st.metric("NCS stock", value='1234', delta='2')
+
+
+#загрузка списка объектов и получение объектов, по которым не ведется работа
+directory = pd.read_excel(r'directory\directory.xlsx', sheet_name='objects_spr')
+status_list = ['Приостановлен', 'Проектирование приостановлено', 'Строительство приостановлено']
+black_objects = directory[directory['status'].isin(status_list)]['obj_key']
+
+#загрузка файла
+
+
+uploaded_file = st.file_uploader("Загрузите файл с КСГ")
+
+
+
+if uploaded_file is not None:
+    with st.spinner("Идет загрузка КСГ..."):
+        df_1 = upload_ksg_1(uploaded_file)
+        
+    st.success('КСГ загрузилось успешно')
+
+    st.metric("Кол-во аномалий, когда срок задачи прошел, а процент завершения 0%", value = f'{df_1.shape[0]}')
+
+    st.write(show_plot_1(df_1))
+    st.dataframe(df_1.astype(str), width=1200, height=400)
+
+    @st.cache_data
+    def download_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+        format1 = workbook.add_format({'num_format': '0.00'}) 
+        worksheet.set_column('A:A', None, format1)  
+        writer.close()
+        processed_data = output.getvalue()
+        return processed_data
+
+    
+    df_xlsx = download_excel(df_1)
+    # st.download_button(
+    #     label="Скачать данные в Excel",
+    #     data=df_xlsx,
+    #     file_name="Аномалии_1-й_группы.xlsx",
+    # )
+    # Код для стилизации кнопки
+    button_style = '''
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #4CAF50;
+        color: white;
+        text-align: center;
+        text-decoration: none;
+        font-size: 16px;
+        border-radius: 4px;
+        cursor: pointer;
+    '''
+
+    # Код для ссылки на скачивание с применением стилей кнопки
+    download_link = f'''
+        <a href="data:application/octet-stream;base64,{base64.b64encode(df_xlsx).decode()}"
+        download="Аномалии_1-й_группы.xlsx" style="{button_style}">
+        Скачать данные в Excel
+        </a>
+    '''
+
+    st.markdown(download_link, unsafe_allow_html=True)
---
+++ modules.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+import xlsxwriter
+import streamlit as st
+
+def upload_ksg_1(uploaded_file):
+    """Данная функция считвает загруженный файл и преобрабатывает данные для вявление аномалий 1-группы
+    Когда срок задачи давно прошел, а процент выполнения задачи 0
+    """
+    
+    df = pd.read_excel(uploaded_file)
+    df['date_report'] = uploaded_file.name.replace('.xlsx', '')
+    df['date_report'] = df['date_report'].apply(pd.to_datetime)
+    df['date_diff'] = (df['ДатаОкончанияЗадачи'] -  df['date_report']).dt.days
+
+    #загрузка списка объектов и получение объектов, по которым не ведется работа
+    directory = pd.read_excel(r'directory\directory.xlsx', sheet_name='objects_spr')
+    status_list = ['Приостановлен', 'Проектирование приостановлено', 'Строительство приостановлено']
+    black_objects = directory[directory['status'].isin(status_list)]['obj_key']
+
+    df = df[~df['obj_key'].isin(black_objects)]
+
+    #убираем бинарные задачи
+    code_task = list(df['Кодзадачи'].unique())
+
+    # уберем бинарные задачи из анализа, то есть, где код задачи всегда 0 или 100
+
+    binary_task = {}
+
+    for task in code_task:
+        
+        values = df[df['Кодзадачи'] == task]['ПроцентЗавершенияЗадачи'].unique()
+        
+        binary_task[task] = values
+
+    tasks_2 = []
+
+    for task in code_task:
+        if len(binary_task[task]) == 2:
+            tasks_2.append(task)
+    
+    df = df[~df['Кодзадачи'].isin(tasks_2)]
+
+    df_past = df[(df['ПроцентЗавершенияЗадачи'] == 0) & (df['date_diff'] < 0)]
+
+    return df_past
+
+
+
+
+def show_plot_1(df: pd.DataFrame):
+    
+    """Данная функция принимает датафрейм и отображает распределение задач на графике"""
+    
+    for_plot = df['date_diff'].value_counts()
+
+    fig, ax = plt.subplots(figsize=(20, 15))
+
+    # Plotting the bar chart with a different color and without edgecolor
+    bars = ax.bar(for_plot.index, for_plot.values, color='lightsteelblue')
+
+    # Customize plot appearance
+    ax.set_xlabel('Отклонение от даты выгрузки до даты завершения задачи', fontsize=18)
+    ax.set_ylabel('Количество записей в КСГ', fontsize=18)
+    ax.set_title('Распределение количества задач по отклонениям от дней выгрузки, где процент завершения задачи 0', fontsize=16)
+
+    # Set x-axis ticks and labels
+    ticks = range(min(for_plot.index.astype(int)), max(for_plot.index.astype(int)), 50)
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks, rotation=45, ha='right')
+
+    # Add grid lines
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+    # Remove the top and right spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    return fig
+
+
+def download_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.close()
+    processed_data = output.getvalue()
+    return processed_data
